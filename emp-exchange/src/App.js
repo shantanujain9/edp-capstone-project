347a296bf9a68a7e9363477943cd124f3c036ada
Shantanu Jain <95145552+shantanujain9@users.noreply.github.com>
2024-07-11 17:11:05 -0400

    Update App.js

--- emp-exchange/src/App.js
+++ emp-exchange/src/App.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
-import {CartProvider} from './contexts/CartContext';
 
 function App() {
   return (
-   
-    <div className="App">
-       <Routes>
-
-            <Route path="/" element ={<LandingPage/>}/>
-            <Route path="/products" component={""}/>
-            <Route path ="/search" component={""}/>
-            <Route path = "/category/:category" component={""}/>
-            <Route path ="/product/:id" component={""}/> 
-      <Route path="/cart" component={Cart}/>
-      <Route path="/checkout" component={Checkout} />
-        </Routes>
-      </div>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">EmpExchange</Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/products">Products</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">Cart</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/products" exact component={ProductList} />
+        <Route path="/product/:id" component={ProductDetail} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/checkout" component={Checkout} />
+      </Switch>
+    </div>
   );
 }
 
