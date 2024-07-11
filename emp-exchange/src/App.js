ad93134a01c206ebe3ed3e71056f3a7f9af62dd5
Alice Smith <alice@webage.com>
2024-07-11 16:33:41 -0400

    fixed merging issues

--- emp-exchange/src/App.js
+++ emp-exchange/src/App.js
@@ -5,17 +5,19 @@ import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
+import {CartProvider} from './contexts/CartContext';
 
 function App() {
   return (
    
     <div className="App">
        <Routes>
+
             <Route path="/" element ={<LandingPage/>}/>
-            {/* <Route path="/products" component={""}/>
+            <Route path="/products" component={""}/>
             <Route path ="/search" component={""}/>
             <Route path = "/category/:category" component={""}/>
-            <Route path ="/product/:id" component={""}/> */}
+            <Route path ="/product/:id" component={""}/> 
       <Route path="/cart" component={Cart}/>
       <Route path="/checkout" component={Checkout} />
         </Routes>
