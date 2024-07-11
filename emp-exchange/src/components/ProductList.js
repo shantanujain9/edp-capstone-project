a41965cbe7908d1dff9f0af92558a70452e0882b
Alice Smith <alice@webage.com>
2024-07-11 15:28:03 -0400

    added backend and created several compoments

---
+++ emp-exchange/src/components/ProductList.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        const fetchProducts = async() =>{
+            const response = await axios.get('http://localhost:5000/products');
+            setProducts(response.data);
+        };
+
+    fetchProducts();
+    }, []);
+
+return (
+    <div className="container">
+        <h2>Products</h2>
+        <div className="row">
+            {products.map(product =>(
+                <div className="col-md-4" key={product._id}>
+                    <div className="card">
+                        <img src={product.image} className="card-img-top" alt={product.name}/>
+                        <div className="card-body">
+                            <h5 className="card-title">{product.name}</h5>
+                            <p className="card-text">{product.description}</p>
+                            <p className="card-text">${product.price}</p>
+                        </div>
+            
+        </div>
+    </div>
+    ))}
+    </div>
+    </div>
+);
+};
+
+export default ProductList;
